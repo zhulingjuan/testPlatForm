295bc6d8ac115521138c6ba00bcf5830ca00fc71
lingjuan.zhu <1425961691@qq.com>
2021-11-25 14:17:46 +0800

    oooo

--- src/layouts/components/VabAvatar/index.vue
+++ src/layouts/components/VabAvatar/index.vue
@@ -10,10 +10,6 @@
     </span>
 
     <el-dropdown-menu slot="dropdown">
-      <el-dropdown-item command="github">github地址</el-dropdown-item>
-      <el-dropdown-item command="gitee" divided>码云地址</el-dropdown-item>
-      <el-dropdown-item command="pro" divided>pro付费版地址</el-dropdown-item>
-      <el-dropdown-item command="plus" divided>plus付费版地址</el-dropdown-item>
       <el-dropdown-item command="logout" divided>退出登录</el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
